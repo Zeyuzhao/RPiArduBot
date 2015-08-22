5973c4c2ff711fdc749830b1f85f4436af3b6c6e
Zach Zhao <zachzhao@outlook.com>
2015-08-22 17:11:42 -0400

    stuff added

--- RPiArduBot.ino
+++ RPiArduBot.ino
@@ -1,25 +1,24 @@
 #include <Motor.h>
 /* A program to interface with python on rpi
- * 
+ *
  * [Device , infoBit1, 2, 3]
  * Motor   A/B    Foward/Back     Val
- * 
+ *
  * [  m    0/1       0/1         0 - 255]
- * 
- * Servo   0 - 180    sweep       
- * 
+ *
+ * Servo   0 - 180    sweep
+ *
  */
 
 
 #define debug 1
-
 
 //communication buffer of 4 bytes
 String getInfo;
 boolean strComplete = false;
 Motor motorA(Motor::MOTOR_A);
 Motor motorB(Motor::MOTOR_B);
-void setup() 
+void setup()
 {
   // Open Serial communication
   Serial.begin(115200);
@@ -35,7 +34,7 @@ void loop() {
     {
       case 'm' : //Motor
         handleMotor(str);
-        
+
         break;
       case 'd' : //Distance sensor
         break;
@@ -70,23 +69,25 @@ void handleMotor(String info)
   int v;
   v = (int)info[3];
   v = v < 0 ? (v + 256) : v; //convert signed char to unsigned
-  //   subtract zero to convert 
+  //   subtract zero to convert
   d = (info[2] - '0') == 0 ? -1 : ((info[2] - '0') == 1 ? 1 : 0); //if direction is 0 then it is foward; if direction is 1 then backward, otherwise zero(no effect)
   //                     multiply to give direction (d is pos or neg)
   (info[1] - '0') == 0 ? motorA.move(v * d) : ((info[1] - '0') == 1 ? motorB.move(v * d) : doNothing()); //if motor is 0 then turn A motor; if motor is 1 then B motor, otherwise do nothing
-  //Serial.println("line: " + info);
-  //Serial.println("Value: " + String(v));
-  //Serial.println("Converted Value: " + String(v * d));
-  //Serial.println("Direction: " + String(info[2]));
-  //Serial.println("Converted Direction: " + String(d));
+
+  #ifndef debug
+  Serial.println("line: " + info);
+  Serial.println("Value: " + String(v));
+  Serial.println("Converted Value: " + String(v * d));
+  Serial.println("Direction: " + String(info[2]));
+  Serial.println("Converted Direction: " + String(d));
+  #endif
 }
 
 void handleServo(String info)
 {
-  
+
 }
 
 void doNothing()
 {
 }
-
